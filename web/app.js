86cff216d4747e8b7e9f3b023750cf26af2723c4
Raihan <17radf@gmail.com>
2023-08-18 19:08:04 +0700

    chore: rebase

--- web/app.js
+++ web/app.js
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var weightRouter = require('./routes/weight');
+var apiRouter = require('./routes/trash_api');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/weight', weightRouter);
+app.use('/api', apiRouter);
 
 // ----------------- just act like this is the end of file -----------------
 
@@ -49,6 +51,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+  // console.log(err)
 });
 
 // listen to the port 8080 
